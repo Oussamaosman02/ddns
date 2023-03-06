69dcacf92f3467620ef92806d871444bab29f3b4
Oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2023-03-06 11:12:22 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -25,17 +25,24 @@ function App () {
   function inversa (mascara, ip, rv) {
     let num
 
-    if (mascara === '8') {
-      num = 3
-    } else if (mascara === '16') {
-      num = 2
-    } else {
-      num = 1
-    }
     if (!rv) {
+      if (mascara === '24') {
+        num = 3
+      } else if (mascara === '16') {
+        num = 2
+      } else {
+        num = 1
+      }
       const inv = ip.split('.').slice(num).reverse().join('.')
       return inv
     } else {
+      if (mascara === '8') {
+        num = 3
+      } else if (mascara === '16') {
+        num = 2
+      } else {
+        num = 1
+      }
       const inv = ip.split('.').reverse().slice(num).join('.')
       return inv
     }
