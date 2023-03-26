efe3cdbf66c31c9f357ff4e2168d910d65a7a69f
oussama02 <oussamaosman02@gmail.com>
2023-03-26 11:05:58 +0200

    Removing unused files and packages

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,3 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFiles: ['./jest.setup.js'],
-  transformIgnorePatterns: [],
-
-  // ModuleNameMapper sólo si ocupamos importar CSS en nuestros componentes para el testing
-  moduleNameMapper: {
-    '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js'
-  }
+  testEnvironment: 'jest-environment-jsdom'
 }
